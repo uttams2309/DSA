9bc2bf49d9af1bd4ee3bb0b8f9a96a9c3f40c85c
uttams2309 <uttams2309@gmail.com>
2025-09-18 20:37:54 +0530

    Intial Publish

---
+++ bfs.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+void solve(){
+    // Your code here
+    int n, m;
+    cin>>n>>m;
+    cout<<n<<m;
+}
+
+signed main(){
+	 #ifndef ONLINE_JUDGE
+	        freopen("input.txt", "r", stdin);
+	        freopen("output.txt", "w", stdout);
+	    #endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+     // int t; cin >> t; while(t--)
+    solve();
+
+    return 0;
+}
